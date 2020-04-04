5c9ec0fec5c9e2bea0525df29a2057a6bc77bbba
Peter V <peter@vavro.me>
2020-04-04 15:35:39 -0500

    Prettier + ESLint https://khalilstemmler.com/blogs/typescript/eslint-for-typescript/ https://khalilstemmler.com/blogs/tooling/prettier/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-    extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            typescript: {},
-        },
-    },
-    rules: {
-        'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
-        '@typescript-eslint/indent': [2, 2],
-        'import/extensions': [2, "never"],
-        '@typescript-eslint/explicit-function-return-type': "off" // disable the rule for all files
-    },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2, // Means error
+    'import/no-extraneous-dependencies': [
+      2,
+      { devDependencies: ['**/test.tsx', '**/test.ts'] }
+    ],
+    '@typescript-eslint/indent': [2, 2],
+    'import/extensions': [2, 'never'],
+    '@typescript-eslint/explicit-function-return-type': 'off' // disable the rule for all files
+  }
 };
